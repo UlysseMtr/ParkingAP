7e98007cb0900e75263575a1afd816e5968baf2b
Ulysse <umetra@mc-media.com>
2025-03-17 15:32:44 +0100

    page liste d'attente simplifié

--- parking-app/resources/views/layouts/app.blade.php
+++ parking-app/resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }} - @yield('title', 'Gestion du Parking')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app-Ho3dDPNA.css') }}">
+    <script src="{{ asset('build/assets/app-CqflisoM.js') }}" defer></script>
 </head>
 
 <body class="h-full">
